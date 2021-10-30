aadd990b5f63ff5bb7ec77d8e447b4fcc0fe369b
bellicose77 <64400354+bellicose77@users.noreply.github.com>
2021-10-30 14:32:28 +0600

    navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import Header from './Pages/Shared/Header/Header';
+import Services from './Pages/Home/Services/Services';
+import Destination from './Pages/Home/Destination/Destination';
+import About from './Pages/Home/About/About';
+import Contact from './Pages/Home/Contact/Contact';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <h2>This is assignment</h2>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/services">
+            <Services></Services>
+          </Route>
+          <Route path="/destination">
+            <Destination></Destination>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/contact">
+            <Contact></Contact>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    const activeStyle = {
+        fontWeight: "bold",
+        color: "red"
+    }
+    return (
+        <>
+            <Navbar bg="dark" variant="dark" sticky="top" collapseOnSelect expand="lg">
+                <Container>
+                    <Navbar.Brand as={NavLink} to="/home" activeStyle={activeStyle} >TourX</Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav.Link as={NavLink} to="/home" activeStyle={activeStyle}>Home</Nav.Link>
+                        <Nav.Link as={NavLink} to="/Services" activeStyle={activeStyle}>Services</Nav.Link>
+                        <Nav.Link as={NavLink} to="/about" activeStyle={activeStyle}>About</Nav.
+                            Link>
+                        <Nav.Link as={NavLink} to="/destination" activeStyle={activeStyle}>Destination</Nav.
+                            Link>
+                        <Nav.Link as={NavLink} to="/contact" activeStyle={activeStyle}>Contact Us</Nav.
+                            Link>
+
+                        {/* {
+                            users?.email ? <Button onClick={logout} variant="light">Logout</Button>
+                                : <Nav.Link as={NavLink} to="/login" activeStyle={activeStyle}>Login</Nav.
+                                    Link>
+                        } */}
+                    </Navbar.Collapse>
+                    <Navbar.Text>
+                        Signed in as: <a href="#login"></a>
+                    </Navbar.Text>
+
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
